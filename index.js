65f202d9802369ca5189e38aa90596a6ae483db6
DRAGOROD <47823363+DRAGOROD@users.noreply.github.com>
2026-02-21 16:49:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+// SELECT DOM ELEMENTS//
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+
+
+
+// PREDEFINED ANIMATION STEPS//
+const animationData = [
+  {
+    inputVal: 5,
+    addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
+  },
+  {
+    inputVal: 2,
+    addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
+  },
+  {
+    inputVal: 1,
+    addElDelay: 2000,
+    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
+  }
+];
+
+
+
+// RECURSIVE DECIMAL → BINARY//
+const decimalToBinary = (input) => {
+
+  if (input === 0 || input === 1) {
+    return String(input);
+  } 
+  else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
+
+// SHOW CALL STACK ANIMATION//
+const showAnimation = () => {
+  result.innerText = "Call Stack Animation";
+
+  animationData.forEach((obj) => {
+
+    // Adding stack frame//
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}" class="animation-frame">
+          decimalToBinary(${obj.inputVal})
+        </p>
+      `;
+    }, obj.addElDelay);
+
+    // Show explanation text//
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
+
+    // Remove stack frame//
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove();
+    }, obj.removeElDelay);
+  });
+
+  //result after animation ends//
+  setTimeout(() => {
+    result.textContent = decimalToBinary(5);
+  }, 20000);
+};
+
+
+
+// INPUT VALIDATION + CONVERSION//
+const checkUserInput = () => {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+  result.textContent = decimalToBinary(inputInt);
+  numberInput.value = "";
+};
+
+
+// Button click with mouse//
+convertBtn.addEventListener("click", checkUserInput);
+
+// Enter key//
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
